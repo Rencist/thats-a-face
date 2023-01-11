d7e6d6d652647c87b3fd803d54b5d50177bf1c92
Rencist <dafianakhdan@gmail.com>
2023-01-11 15:33:21 +0700

    feat: face detector of image

--- That_A_Face.py
+++ That_A_Face.py
@@ -3,8 +3,12 @@ import cv2
 trained_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread('dafian.png')
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+face_coordinates = trained_face.detectMultiScale(gray_img)
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+print(face_coordinates)
 
-cv2.imshow('Dafian', gray_img)
+cv2.imshow('Dafian', img)
 cv2.waitKey()
 
 print("Lmao Complete")
