717f915f19e9eb71479e8f129589d73074407014
Rencist <dafianakhdan@gmail.com>
2023-01-11 15:42:28 +0700

    feat: face detector in webcam video

--- That_A_Face.py
+++ That_A_Face.py
@@ -1,14 +1,24 @@
 import cv2
 
 trained_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-img = cv2.imread('dafian.png')
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-face_coordinates = trained_face.detectMultiScale(gray_img)
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
-print(face_coordinates)
+# img = cv2.imread('dafian.png')
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# face_coordinates = trained_face.detectMultiScale(gray_img)
+# for (x, y, w, h) in face_coordinates:
+#     cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+# print(face_coordinates)
 
-cv2.imshow('Dafian', img)
-cv2.waitKey()
+# cv2.imshow('Dafian', img)
+# cv2.waitKey()
 
-print("Lmao Complete")
+webcam = cv2.VideoCapture(0)
+while True:
+    success_frame, frame = webcam.read()
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face.detectMultiScale(gray_img)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+    print(face_coordinates)
+
+    cv2.imshow('That A Face', frame)
+    cv2.waitKey(1)
