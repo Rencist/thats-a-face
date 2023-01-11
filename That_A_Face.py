6164359be74bae9b2791a55c85692e496b23952d
Rencist <dafianakhdan@gmail.com>
2023-01-11 15:05:34 +0700

    fix: change black and white then show image

--- That_A_Face.py
+++ That_A_Face.py
@@ -2,9 +2,9 @@ import cv2
 
 trained_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread('dafian.png')
-cv2.imshow('Dafian', img)
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+cv2.imshow('Dafian', gray_img)
 cv2.waitKey()
 
 print("Lmao Complete")
